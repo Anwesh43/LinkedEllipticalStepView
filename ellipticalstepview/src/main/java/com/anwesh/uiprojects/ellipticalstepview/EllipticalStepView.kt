79df686b2cff90ed64105896b81988a45fc5b04d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-08 01:12:50 +0530

    adding drawESNode canvas extension method

--- ellipticalstepview/src/main/java/com/anwesh/uiprojects/ellipticalstepview/EllipticalStepView.kt
+++ ellipticalstepview/src/main/java/com/anwesh/uiprojects/ellipticalstepview/EllipticalStepView.kt
@@ -9,10 +9,32 @@ import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Path
+import android.graphics.RectF
 import android.content.Context
 
 val nodes : Int = 5
+
+fun Canvas.drawESNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val wSize : Float = 2 * gap/3
+    paint.color = Color.parseColor("#43A047")
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap + i * gap, h/2)
+    for (j in 0..1) {
+        val sf : Float = 1f - 2 * j
+        val sc : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f * j)) * 2
+        save()
+        scale(1f, sf)
+        drawArc(RectF(-wSize/2, -wSize/5, wSize/2, wSize/5), 180f, 180f * sc, false, paint)
+        restore()
+    }
+    restore()
+}
 
 class EllipticalStepView(ctx : Context) : View(ctx) {
 
