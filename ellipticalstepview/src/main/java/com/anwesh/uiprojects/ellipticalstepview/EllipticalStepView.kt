6c3192bda6df9ccf0ea1267ad41d03b7450ab763
Anwesh43 <anweshthecool0@gmail.com>
2018-10-08 00:51:42 +0530

    adding libraries and defining nodes

--- ellipticalstepview/src/main/java/com/anwesh/uiprojects/ellipticalstepview/EllipticalStepView.kt
+++ ellipticalstepview/src/main/java/com/anwesh/uiprojects/ellipticalstepview/EllipticalStepView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.ellipticalstepview
 /**
  * Created by anweshmishra on 08/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+import android.content.Context
+
+val nodes : Int = 5
+
